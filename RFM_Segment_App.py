039c735d6a94b7cf244f38194e24b5900d807c30
lethongnhut <158292451+lethongnhut@users.noreply.github.com>
2024-06-23 13:55:20 +0700

    Update RFM_Segment_App.py

--- RFM_Segment_App.py
+++ RFM_Segment_App.py
@@ -513,6 +513,16 @@ elif choice == 'Phân cụm khách hàng':
     st.write('Download file csv đã phân nhóm khách hàng theo RFM')
     # csv = df_RFM_rule.to_csv(index=False).encode('utf-8')
 ###############################################################################
+    # Kiểm tra kiểu dữ liệu
+    print(df_RFM_rule.info())
+
+    # Chuyển đổi kiểu dữ liệu (ví dụ)
+    for col in df_RFM_rule.columns:
+        if df_RFM_rule[col].dtype not in ['int64', 'float64', 'object']:
+            df_RFM_rule[col] = df_RFM_rule[col].astype(str)
+
+    # Xử lý giá trị NaN
+    df_RFM_rule = df_RFM_rule.fillna('')
     # Kiểm tra kiểu dữ liệu của các cột
     print(df_RFM_rule.info())
 
